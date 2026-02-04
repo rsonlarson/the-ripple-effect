f7a36711b2927822f1fe608b50eabcaaddbeedcc
Soren Larson <156987111+rsonlarson@users.noreply.github.com>
2026-02-04 16:34:48 -0700

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,7 +13,7 @@ export const projects: Project[] = [
     _id: 'Prjct_1',
     projectName: 'Mugs for Many',
     shortSummary: 'Bring a Mug, Take a Mug.\n \nProviding a space to give and take. Give a mug a new home and spread the joy of sharing',
-    description: 'This project is built around a simple idea: one small act can make a difference. By sharing and reusing mugs, we reduce waste, save resources, and create something quietly meaningful together. Bringing or taking a mug may seem small, but those small choices add up. Warmth in your hands, warmth in your drink, and warmth in the shared effort to care a little more for our community and the environment. \n If you are interested in letting us implement this idea in your own place of business or some business near you, please email us at the contact below. We take care of setting up the basket, mugs, sign and more. If you have any questions, we are always ready to assist!',
+    description: 'This project is built around a simple idea: one small act can make a difference. By sharing and reusing mugs, we reduce waste, save resources, and create something quietly meaningful together. Bringing or taking a mug may seem small, but those small choices add up. Warmth in your hands, warmth in your drink, and warmth in the shared effort to care a little more for our community and the environment. \n If you are interested in letting us implement this idea in your own place of business or a business near you, please email us at the contact below. We take care of setting up the basket, mugs, sign and more. If you have any questions, we are always ready to assist!',
     slogan: 'One small act, Less waste, More warmth',
     projectImage: '',
     isActive: true,
